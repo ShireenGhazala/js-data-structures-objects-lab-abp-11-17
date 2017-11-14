09cea04869b596cd39aa18c4e3edaa88d8726242
Shireen Ghazala <shireen.gaz@gmail.com>
2017-11-14 06:59:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,9 @@ function deleteFromDriverByKey(driver,key){
   delete newDriver.key;
   return newDriver.name;
 }
+
+function destructivelyDeleteFromdriverByKey(driver,key){
+  delete driver.key;
+  return driver;
+  
+}
