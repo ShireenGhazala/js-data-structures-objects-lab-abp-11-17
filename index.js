6e917ad93717c517c359ac7728b0dd041ec8304b
Shireen Ghazala <shireen.gaz@gmail.com>
2017-11-14 06:56:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
 }
 function deleteFromDriverByKey(driver,key){
   const newDriver=Object.assign({}, driver);
-  delete driver.key
+  delete driver.key;
 }
